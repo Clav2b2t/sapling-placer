a95ad77e940dcf4549007461b5376a794ef38e59
Clav2b2t <clavicusvile2b2t@gmail.com>
2023-09-27 16:07:50 -0500

    set up complete

---
+++ src/main/java/net/clav/saplingplacer/SaplingPlacerClient.java
@@ -0,0 +1,10 @@
+package net.clav.saplingplacer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SaplingPlacerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/clav/saplingplacer/SaplingPlacerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.clav.saplingplacer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SaplingPlacerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
